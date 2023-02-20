cf8f5a438c42cacbf5693cf35a6af78a5da83312
ekomobong udonwa <ekomobongudonwa@gmail.com>
2023-02-19 21:44:17 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - the alphabet game
+ * Return: 0
+ */
+int main(void)
+{
+	char bn;
+
+	for
+		(bn = 'a';
+		bn <= 'z';
+		bn++);
+	{
+		putchar(bn);
+	}
+	putchar('\n');
+	return (0);
